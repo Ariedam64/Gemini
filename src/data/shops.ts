aaf5990514c88d8132511490c38a7fb8a75ce8a7
Ariedam64 <manitou88.jeux@gmail.com>
2026-01-23 08:54:26 +0100

    Remove unecessary files

--- src/data/shops.ts
+++
@@ -1,7 +0,0 @@
-// shops.ts
-export const shops = {
-  seed:  { reset: 300 },
-  tool:  { reset: 600 },
-  egg:   { reset: 900 },
-  decor: { reset: 3600 },
-} as const satisfies Record<string, { reset: number }>;
