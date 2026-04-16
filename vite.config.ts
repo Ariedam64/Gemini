00008eb442af9210439186ad3836425aaec78d8f
Ariedam64 <manitou88.jeux@gmail.com>
2026-04-16 09:12:45 +0200

    Update test

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig(({ mode }) => ({
             userscript: {
                 name: 'Gemini',
                 namespace: 'Gemini',
-                version: '1.0.1',
+                version: '1.0.2',
                 match: [
                     'https://1227719606223765687.discordsays.com/*',
                     'https://magiccircle.gg/r/*',
